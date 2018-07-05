20675291455e388c32c8b372beadf185def3afbb
mike <miketz@users.noreply.github.com>
2018-07-05 10:17:02 -0400

    use symlinks instead of hard copies.

--- install.sh
+++ install.sh
@@ -3,10 +3,36 @@
 #   sudo sh ./install.sh
 ##########################################
 
+# cp ./rc.xml /etc/X11/openbox/rc.xml
+# cp ./.vimrc ~/.vimrc
+# cp ./.tmux.conf ~/.tmux.conf
+# cp ./.bash_aliases ~/.bash_aliases
+
+dir=~/dotfiles
+dirbackup=~/dotfiles_BACKUP
+
+mkdir -p $dirbackup
+cd $dir
+
+
 # on wild-dog
-cp ./rc.xml /etc/X11/openbox/rc.xml
+mv /etc/X11/openbox/rc.xml $dirbackup
+ln -s $dir/rc.xml /etc/X11/openbox/rc.xml
 
+mv ~/.conkerorrc $dirbackup
+ln -s $dir/.conkerorrc ~/.conkerorrc
 
-cp ./.vimrc ~/.vimrc
-cp ./.tmux.conf ~/.tmux.conf
-cp ./.bash_aliases ~/.bash_aliases
+mv ~/.ratpoisonrc $dirbackup
+ln -s $dir/.ratpoisonrc ~/.ratpoisonrc
+
+mv ~/.stumpwmrc $dirbackup
+ln -s $dir/.stumpwmrc ~/.stumpwmrc
+
+mv ~/.vimrc $dirbackup
+ln -s $dir/.vimrc ~/.vimrc
+
+mv ~/.tmux.conf $dirbackup
+ln -s $dir/.tmux.conf ~/.tmux.conf
+
+mv ~/.bash_aliases $dirbackup
+ln -s $dir/.bash_aliases ~/.bash_aliases
